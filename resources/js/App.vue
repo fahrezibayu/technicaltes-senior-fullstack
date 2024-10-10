f8fc4807afd8121446edf10a3f27a846073b273e
fahrezibayu <fbp.mj32@gmail.com>
2024-10-10 09:19:41 +0700

    update tampilan

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -28,7 +28,7 @@
                         v-for="item in navigation"
                         :key="item.name"
                         :to="item.href"
-                        class="text-sm font-semibold leading-6 text-gray-900"
+                        class="text-sm font-semibold leading-6 text-gray-900 hover:text-blue-600"
                     >
                         {{ item.name }}
                     </router-link>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('app');
 });
+Route::get('/petani', function () {
+    return view('app');
+});
+Route::get('/fasilitator', function () {
+    return view('app');
+});
